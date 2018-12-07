5df93ebea57153a202286848f078c8c5eb822e61
STA2UA <32482440+STA2UA@users.noreply.github.com>
2018-12-07 18:14:39 +0100

    Update controll.php

--- controll.php
+++ controll.php
@@ -1,7 +1,17 @@
 <?php
+	$order = $_GET["order"];
+	$value = $_GET["value"];
 	$controll_file = 'order.txt';
 
-	$order = file_get_contents($controll_file);
-	file_put_contents($controll_file, "");
-	echo $order;
+	if ($order == "set") {
+		file_put_contents($controll_file, "");
+	}
+
+	if ($order == "get") {
+		$order = file_get_contents($controll_file);
+		file_put_contents($controll_file, $value);
+		echo $order;
+	}
+
+
 ?>
